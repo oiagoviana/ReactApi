6cfbf6f5c3308d17e1e1bc6a5d164cc6106dc4d1
Rodrigo <rodrigo.anjos0806@gmail.com>
2022-05-25 17:18:17 -0300

    saasasas

--- src/pages/maiorNumero/index.js
+++ src/pages/maiorNumero/index.js
@@ -1,10 +1,40 @@
-
+import axios from "axios";
+import { useState } from 'react'
 
 export default function Index(){
+    const [numeros, setNumeros] = useState([]);
+    const [numero, setNumero] = useState(0);
+    const [resposta, setReposta] = useState('');
+
+    function adicionar () {
+        setNumeros([...numeros, numero]);
+        setNumero('');
+    }
+
+    async function verificarMaior () {
+        const resp = await axios.post('http://localhost:5000/dia2/maiornumero', numeros);
+        setReposta(resp.data.maior);
+    }
+
     return (
         <main>
             <h1>Maior Número</h1>
+
+            <div>
+                Número: <input type="text" value={numero} onChange= {e => setNumero(Number(e.target.value))}/>
+                <button onClick={adicionar}>Adicionar Número</button>
+            </div>
            
+           <div>
+               {numeros.map(item =>
+                <div>{item}</div>
+                )}
+           </div>
+
+           <div>
+               <button onClick={verificarMaior}>Verificar</button> Maior número é {resposta}
+           </div>
         </main>
     )
 }
+
